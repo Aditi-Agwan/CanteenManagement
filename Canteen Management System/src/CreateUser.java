4f7a2343f69caa170d3c13ffaeaffe81edb8ad33
Aditi Agwan <agwanaditi02@gmail.com>
2022-07-18 16:33:27 +0530

    User authentication done

--- Canteen Management System/src/CreateUser.java
+++ Canteen Management System/src/CreateUser.java
@@ -10,6 +10,7 @@ public static String userCreation(String username, String password) {
 	if(user_username.contains(username)) {
 		return "Username already exists";
 	}
+	//Password Checker according to condition
 	int contain_digit =0;
 	int contain_uppercase =0;
 	int contain_specialCharacter =0;
@@ -18,96 +19,48 @@ public static String userCreation(String username, String password) {
 	if(password.length()>=8) {
 		for(int i=0;i<password.length();i++) {
 			int Ascii = (int)password.charAt(i);
-			if(Ascii>=)
-		}
-		for(int i=0;i<9;i++) {
-			if(password.contains(Integer.toString(i))) {
-				contain_digit=1;
-				break;
-			}
-			    
-		}
-		if(contain_digit ==0)
-			userOutput += "\n Password must contain atleast a digit";
-		
-			for(int i=65;i<91;i++) {
-				char ch = (char)i;
-				String uppercaseLetter = Character.toString(ch);
-				
-				if(password.contains(uppercaseLetter)) {
-					contain_uppercase =1;
+			if(Ascii>=65 && Ascii<=90)
+				contain_uppercase =1;
+			if(Ascci>=48 && Ascii<=57)
+				contain_digit =1;
+			if((Ascii>=32 && Ascii<=47) || (Ascii>=58 && Ascii<=64) || (Ascii>=91 && Ascii<=96) || (Ascii>=123 &&Ascii<=126>))
+				contain_specialCharacter =1;
+			
+			int count =1;
+			for(int j=i+1;j<password.length();j++) {
+				if(password.charAt(j) == password.charAt(i))
+					count++;
+				if(count>3) {
+					userOutput +=  "\n A letter "+ password.charAt(i) + " should not repeat more than 3 times";
+					contain_repeatChar =1;
 					break;
 				}
 					
 			}
-			
-			if(contain_uppercase ==0)
-			userOutput += "\n Password must contain atleast 1 uppercase letter ";
+
+		}
 		
-			for(int i=32;i<48;i++) {
-				char ch = (char)i;
-				String specialCharacter = Character.toString(ch);
-				
-				if(password.contains(specialCharacter)) {
-					contain_specialCharacter =1;
-					break;
-			}
-		}
-			for(int i=58;i<65;i++) {
-				char ch = (char)i;
-				String specialCharacter = Character.toString(ch);
-				
-				if(password.contains(specialCharacter)) {
-					contain_specialCharacter =1;
-					break;
-			}
-		}
-			for(int i=91;i<97;i++) {
-				char ch = (char)i;
-				String specialCharacter = Character.toString(ch);
-				
-				if(password.contains(specialCharacter)) {
-					contain_specialCharacter =1;
-					break;
-			}
-		}	
-			for(int i=123;i<127;i++) {
-				char ch = (char)i;
-				String specialCharacter = Character.toString(ch);
-				
-				if(password.contains(specialCharacter)) {
-					contain_specialCharacter =1;
-					break;
-			}
-		}
-			if(contain_specialCharacter ==0)
-				userOutput += "\n Password must contain atleast 1 special case ";
-			for(int i=0;i<password.length();i++) {
-				int count =1;
-				for(int j=i+1;j<password.length();j++) {
-					if(password.charAt(j) == password.charAt(i))
-						count++;
-					if(count>3) {
-						userOutput +=  "\n A letter should not repeat more than 3 times";
-						contain_repeatChar =1;
-						break;
-					}
-						
-				}
-			}	
-		if(contain_digit ==1 && contain_uppercase  ==1 && contain_specialCharacter ==1 && contain_repeatChar==0) {
 			
+		if(contain_digit ==1 && contain_uppercase  ==1 && contain_specialCharacter ==1 && contain_repeatChar==0) {
 			
 			password = passwordEncryptor(password);
 			user_username.add(username);
 			user_password.add(password);
 			user_account.put(username,password);
 			System.out.println(password);
-			return null;
+			userOutput+="User Account created";
+		}
+		else {
+			if(contain_digit ==0)
+				userOutput+= "Password must contain atleast 1 digit";
+			if(contain_uppercase  ==0)
+				userOutput+= "Password must contain atleast 1 uppercase letter";
+			if(contain_specialCharacter ==0)
+				userOutput+= "Password must contain atleast 1 special case character";
 		}
 	  }
 	  else {
-		  return "Password length must be atleast 8 characters";
+		  userOutput+= "Password length must be atleast 8 characters";
 	  }
 	   
 	return userOutput;
